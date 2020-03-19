53c6f4ef6331b25d66afa05df1f94362359304de
CommonMatter <hoffmelanie@gmail.com>
2020-03-19 01:00:00 -0400

    not defined err

--- src/components/MainElements/GetMainElements.js
+++ src/components/MainElements/GetMainElements.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './GetOnlinePosts.css';
+import './GetMainElements.css';
 class GetOnlinePosts extends Component {
     constructor(props){
         super(props);
@@ -10,7 +10,7 @@ class GetOnlinePosts extends Component {
         };
     }
     componentDidMount() {
-        fetch(".netlify/functions/lambda")
+        fetch(".netlify/functions/main")
       .then( response => response.json())
           .then( (data) => {
             this.setState({
@@ -28,44 +28,14 @@ class GetOnlinePosts extends Component {
       console.log(err)
     });
     }
-  render() {
-      // An alternative way to do this that's preferred in most style guides is:
-        // const { records } = this.state;
-        // See the object destructuring section: 
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-      const { records } = this.state;
-        // This map call produces an array of <div> elements. Alternatively, we could use another
-        // type of loop such a for-loop to do this, but React style guides prefer these
-        // functional approaches.
-        const committeeElements = records.map(record => {
-            // An alternative way to do this that's preferred in most style guides is:
-            // const { fields, id } = record;
-            const id = record.id;
-            const fields = record.fields;
-            // You can also rename in the destructuring call with:
-            // const {Description: description, Committees: committees} = fields;
-            // https://wesbos.com/destructuring-renaming/
-            const description = fields.Description;
-            const committee = fields.Committees;
-            // Here, we are taking the data we pulled from the object and rendering them
-            // with a JSX element!
-            // Note that you need to provide a unique key for each element that you create
-            // through an loop like this or React will give a warning.
-            return (
-                <div key={id}>
-                    <p>{committee}</p>
-                    <p>{description}</p>
+    render() {
+
+            return(
+                <div>
+                    
                 </div>
             );
-        });
-        // We're rendering the array of committeeElements in our final output!
-        // Instead of rendering the variable, we could also have put the entire map call
-        // into the brackets inside of this div to produce the same effect. Up to you!
-        return(
-            <div>
-                {committeeElements}
-            </div>
-        );
+        }
     }
-  }
-  export default GetOnlinePosts;
+    
+  export default GetMainElements;
