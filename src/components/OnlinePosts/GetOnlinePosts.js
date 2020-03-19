95f8a7afaa0284485c01db68431589013ca4b344
CommonMatter <hoffmelanie@gmail.com>
2020-03-18 22:13:14 -0400

    bomani helped render data on the page😭<3

--- src/components/OnlinePosts/GetOnlinePosts.js
+++ src/components/OnlinePosts/GetOnlinePosts.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 class GetOnlinePosts extends Component {
     constructor(props){
         super(props);
@@ -9,46 +8,63 @@ class GetOnlinePosts extends Component {
             records : []        
         };
     }
-
     componentDidMount() {
-        fetch(".netlify/functions/lambda")
-		  .then( response => response.json())
-          .then( (data) => {
+        fetch(".netlify/functions/lambda")
+      .then( response => response.json())
+          .then( (data) => {
             this.setState({
                 isLoaded : true,
                 meow : data,
                 records: data.records
             });
-          console.log(data.records)
-
-        })
-			.catch(err => {
-			 this.setState({
+          console.log('Mount Records', data.records)
+        })
+      .catch(err => {
+       this.setState({
                 isLoaded: true,
                 err
             });
-			console.log(err)
-		});
-    }
-
-	render() {
-     	// const {err, isLoaded, records} = this.state;
-     	//console.log(this.state.records)
-     	var records = this.state.records;
-     	console.log(records[0])
-
-
-            return(
-            	<div>
-            		{}
-            	</div>
-
-            );
-        
-      
+      console.log(err)
+    });
     }
-
-
+  render() {
+      // An alternative way to do this that's preferred in most style guides is:
+        // const { records } = this.state;
+        // See the object destructuring section: 
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+      const { records } = this.state;
+        // This map call produces an array of <div> elements. Alternatively, we could use another
+        // type of loop such a for-loop to do this, but React style guides prefer these
+        // functional approaches.
+        const committeeElements = records.map(record => {
+            // An alternative way to do this that's preferred in most style guides is:
+            // const { fields, id } = record;
+            const id = record.id;
+            const fields = record.fields;
+            // You can also rename in the destructuring call with:
+            // const {Description: description, Committees: committees} = fields;
+            // https://wesbos.com/destructuring-renaming/
+            const description = fields.Description;
+            const committee = fields.Committees;
+            // Here, we are taking the data we pulled from the object and rendering them
+            // with a JSX element!
+            // Note that you need to provide a unique key for each element that you create
+            // through an loop like this or React will give a warning.
+            return (
+                <div key={id}>
+                    <p>{committee}</p>
+                    <p>{description}</p>
+                </div>
+            );
+        });
+        // We're rendering the array of committeeElements in our final output!
+        // Instead of rendering the variable, we could also have put the entire map call
+        // into the brackets inside of this div to produce the same effect. Up to you!
+        return(
+            <div>
+                {committeeElements}
+            </div>
+        );
+    }
   }
-  
   export default GetOnlinePosts;
