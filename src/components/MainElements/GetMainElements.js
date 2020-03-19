a723977a485e56a297e8d894d6a23c3d214824e9
CommonMatter <hoffmelanie@gmail.com>
2020-03-19 03:12:46 -0400

    main elements rendered

--- src/components/MainElements/GetMainElements.js
+++ src/components/MainElements/GetMainElements.js
@@ -18,7 +18,7 @@ class GetMainElements extends Component {
                 meow : data,
                 records: data.records
             });
-          console.log('Mount Records', data.records)
+          console.log('Main Records', data.records)
         })
       .catch(err => {
        this.setState({
@@ -28,14 +28,45 @@ class GetMainElements extends Component {
       console.log(err)
     });
     }
-    render() {
-
-            return(
-                <div>
-                    
+  render() {
+      // An alternative way to do this that's preferred in most style guides is:
+        // const { records } = this.state;
+        // See the object destructuring section: 
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+      const { records } = this.state;
+        // This map call produces an array of <div> elements. Alternatively, we could use another
+        // type of loop such a for-loop to do this, but React style guides prefer these
+        // functional approaches.
+        const mainElements = records.map(record => {
+            // An alternative way to do this that's preferred in most style guides is:
+            // const { fields, id } = record;
+            const id = record.id;
+            const fields = record.fields;
+            // You can also rename in the destructuring call with:
+            // const {Description: description, Committees: committees} = fields;
+            // https://wesbos.com/destructuring-renaming/
+            const text = fields.Text;
+            const title = fields.Title;
+            // Here, we are taking the data we pulled from the object and rendering them
+            // with a JSX element!
+            // Note that you need to provide a unique key for each element that you create
+            // through an loop like this or React will give a warning.
+            return (
+                <div id="MainElements" key={id}>
+                        <h3>{title}</h3>
+                        <p>{text}</p>
                 </div>
             );
-        }
+        });
+        // We're rendering the array of committeeElements in our final output!
+        // Instead of rendering the variable, we could also have put the entire map call
+        // into the brackets inside of this div to produce the same effect. Up to you!
+        return(
+            <div id="MainElements">
+                {mainElements}
+            </div>
+        );
+    }
     }
 
   export default GetMainElements;
