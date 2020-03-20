e3bdee9db71acc62215ef46fa018a445a476adcf
CommonMatter <hoffmelanie@gmail.com>
2020-03-20 13:19:10 -0400

    syncing with airtable

--- src/App.js
+++ src/App.js
@@ -3,33 +3,13 @@ import React from 'react';
 // import poster from '../public/cloud9.png';
 import './App.css';
 import GetOnlinePosts from './components/OnlinePosts/GetOnlinePosts';
+import GetMainElements from './components/MainElements/GetMainElements';
 
 const pub = process.env.PUBLIC_URL;
 
 class App extends React.Component {
 
-componentDidMount() {
-        fetch(".netlify/functions/lambda")
-        .then(function(response) {
-//           console.log(response)
-          return response.json();
-        })
-          .then( (data) => {
-          //omits projects tagged as private
-            this.setState({
-                isLoaded : true,
-                posts : data
-            });
-//           console.log(data)
 
-        }).catch(err => {
-			 this.setState({
-                isLoaded: true,
-                err
-            });
-			console.log(err)
-		});
-    }
 
 render() {
 
@@ -72,59 +52,9 @@ render() {
 
 
       {/* NAVIGATION */}
-      <div id="MainElements" >
 
-        <a href="#About">
-          <div id="MainElements" class="about-bubble bubble Blue-Outline">
-            <div>
-            <h5 class="light">About</h5>
-            <p>Join Us &amp; lets share stories, strategies, wisdom, sweetness, &amp; online space Now is a time fortify our networks of care &lt;3 We will be holding free online programs with you for you. <u><i>Learn more!</i></u></p>
-            </div>
-          </div>
-        </a>
+      <GetMainElements/>
 
-        <a href="">
-          <div id="MainElements" class="donate-bubble bubble Green-Outline">
-            <div>
-            <h5 class="light">Donate</h5>
-            <p>We are gathering funds // taking donations in these sessions to pay // uplift community who are being impacted by this pandemic. <u><i>Support!</i></u></p>
-            </div>
-          </div>
-        </a>
-
-        <a href="#Guidelines">
-          <div id="MainElements" class="guidelines-bubble Orange-Outline bubble">
-            <div>
-            <h5 class="light">Community Guidelines</h5>
-            <p>We are crowdsourcing community agreements with our Cloud9 community to make sure our efforts during this virus are as healthy, honest and materially constructive as they would be in any physical space we would hold together &lt;33</p>
-            </div>
-          </div>
-        </a>
-
-        <a href="#Committees">
-          <div id="MainElements" class="contribute-bubble bubble Pink-Outline">
-            <h5 class="light">Contribute</h5>
-          </div>
-        </a>
-
-        <a href="#Resources">
-          <div id="MainElements" class="resources-bubble bubble Red-Outline">
-            <h5 class="light">Resources</h5>
-          </div>
-        </a>
-
-        <a href="#Calendar">
-          <div id="MainElements" class="calendar-bubble bubble Yellow-Outline">
-            <h5 class="light">Calendar</h5>
-          </div>
-        </a>
-
-      </div>
-
-
-
-
-      {/* <GetMainElements/> */}
       </div>
 
 
--- src/components/MainElements/GetMainElements.js
+++ src/components/MainElements/GetMainElements.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../components.css';
-class GetOnlinePosts extends Component {
+class GetMainElements extends Component {
     constructor(props){
         super(props);
         this.state = {
@@ -18,7 +18,7 @@ class GetOnlinePosts extends Component {
                 meow : data,
                 records: data.records
             });
-          console.log('Mount Records', data.records)
+          console.log('Main records', data.records)
         })
       .catch(err => {
        this.setState({
@@ -37,7 +37,7 @@ class GetOnlinePosts extends Component {
         // This map call produces an array of <div> elements. Alternatively, we could use another
         // type of loop such a for-loop to do this, but React style guides prefer these
         // functional approaches.
-        const committeeElements = records.map(record => {
+        const mainElements = records.map(record => {
             // An alternative way to do this that's preferred in most style guides is:
             // const { fields, id } = record;
             const id = record.id;
@@ -45,27 +45,27 @@ class GetOnlinePosts extends Component {
             // You can also rename in the destructuring call with:
             // const {Description: description, Committees: committees} = fields;
             // https://wesbos.com/destructuring-renaming/
-            const description = fields.Description;
-            const committee = fields.Committees;
+            const text = fields.Text;
+            const title = fields.Title;
             // Here, we are taking the data we pulled from the object and rendering them
             // with a JSX element!
             // Note that you need to provide a unique key for each element that you create
             // through an loop like this or React will give a warning.
             return (
                 <div key={id}>
-                    <p>{committee}</p>
-                    <p>{description}</p>
+                    <p>{title}</p>
+                    <p>{text}</p>
                 </div>
             );
         });
-        // We're rendering the array of committeeElements in our final output!
+        // We're rendering the array of mainElements in our final output!
         // Instead of rendering the variable, we could also have put the entire map call
         // into the brackets inside of this div to produce the same effect. Up to you!
         return(
             <div>
-                {committeeElements}
+                {mainElements}
             </div>
         );
     }
   }
-  export default GetOnlinePosts;
+  export default GetMainElements;
