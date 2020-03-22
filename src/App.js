d13861ccd4199d2889961507c91036684697d18b
CommonMatter <hoffmelanie@gmail.com>
2020-03-22 12:55:44 -0400

    data coming from app.js thanks bomanii

--- src/App.js
+++ src/App.js
@@ -44,28 +44,11 @@ render() {
           <img src={pub + 'cloud9-logo.png'} />
         </header>
 
-        {/* CALENDAR KEY */}
-        {/* <div class="Key">
-            <div class="keyLine">
-              <div class="bullet Red-Fill"></div> <span> Community Check-ins</span>
-              <div class="bullet Orange-Fill"></div> <span> Meditations</span>
-            </div>
-            <div class="keyLine">
-              <div class="bullet Yellow-Fill"></div> <span> Skillshares</span>
-              <div class="bullet Green-Fill"></div> <span> Movement</span>
-              <div class="bullet Blue-Fill"></div> <span> Nightlife</span>
-            </div>
-            <div class="keyLine">
-              <div class="bullet Purple-Fill"></div> <span> Frontline PSAs</span>
-              <div class="bullet Pink-Fill"></div> <span> Digital Security</span>
-            </div>
-        </div> */}
-
       {/* CONTENT */}
       <div className="cloudbg">
       </div>
       <div className="Content">
-      <GetMainElementsInfo record={records} />
+      <GetMainElementsInfo records={records} />
       <GetOnlinePosts/>
       </div>
       {/* NAVIGATION */}
