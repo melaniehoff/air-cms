65e216d2bb6ec3737c6cd730554c7a771cb4a37d
CommonMatter <hoffmelanie@gmail.com>
2020-03-23 22:12:04 -0400

    got new calendar page

--- src/App-pre-route.js
+++ src/App-pre-route.js
@@ -9,7 +9,6 @@ import './App.css';
 import GetOnlinePosts from './components/OnlinePosts/GetOnlinePosts';
 import GetMainElements from './components/MainElements/GetMainElements';
 import GetMainElementsInfo from './components/MainElementsInfo/GetMainElementsInfo';
-import GetCalendar from './components/Calendar/GetCalendar';
 const pub = process.env.PUBLIC_URL;
 class App extends React.Component {
   constructor(props){
@@ -43,50 +42,36 @@ render() {
     const { records } = this.state;
     return (
       <Router>
-        <div className="App">
-          {/* LOGO */}
-          <header className="App-header">
-            <img src={pub + 'cloud9-logo.png'} />
-          </header>
-          {/* CALENDAR SEPARATE PAGE */}
-          <nav>
-                <Link to="/calendar">Calendar</Link>
-          </nav>
-          {/* CONTENT */}
+      <div className="App">
+
+        {/* LOGO */}
+        <header className="App-header">
+          <img src={pub + 'cloud9-logo.png'} />
+        </header>
+        <nav>
+              <Link to="/calendar">Calendar</Link>
+        </nav>
+
+      {/* CONTENT */}
+      <div className="cloudbg">
+      </div>
+      <div className="Content">
+      <GetMainElementsInfo records={records} />
+      <GetOnlinePosts/>
+      </div>
+      {/* NAVIGATION */}
+      <GetMainElements records={records}/>
 
 
-          {/* NAVIGATION */}
+
           <Switch>
-            <Route path="/calendar">
-              <Calendar />
-            </Route>
-            <Route path="/">
-              <Home records={records} />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+          <Route path="/calendar">
+          
+          </Route>
+        </Switch>
+      </div>
+</Router>
     );
   }
-}
-function Home(props) {
-  return (
-    <div>
-      <div className="cloudbg">
-        </div>
-        <div className="Content">
-          <GetMainElementsInfo records={props.records} />
-          <GetOnlinePosts/>
-         =<GetMainElements records={props.records}/>
-        </div>
-      </div>
-  );
-}
-function Calendar() {
-  return (
-    <div className="cloudbg">
-      <GetCalendar/>
-    </div>
-  );
 }
 export default App;
--- src/components/Calendar/GetCalendar.js
+++ src/components/Calendar/GetCalendar.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import '../Components.css';
+import Media from 'react-media';
 
 class GetCalendar extends Component {
+
   render() {
 
-        const calendar = {
-
-            const test = "TESTTESTTEST";
-
-            return (
-                <div key={test}>
-                    <p>{test}</p>
-                </div>
-            );
-        });
         return(
-            <div>
-                {calendar}
-            </div>
+        
+  
+              <p>CLOU9 CALENDAR</p>
+                  
+             
+
         );
     }
-    }
+  }
   export default GetCalendar;
