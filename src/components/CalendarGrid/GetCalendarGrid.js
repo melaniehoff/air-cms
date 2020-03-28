d7e2557b306ebdfa51edf77d3b2bdfcc72dbbda9
CommonMatter <hoffmelanie@gmail.com>
2020-03-28 19:01:51 -0400

    cleanup comments

--- src/components/CalendarGrid/GetCalendarGrid.js
+++ src/components/CalendarGrid/GetCalendarGrid.js
@@ -213,67 +213,6 @@ class GetCalendarGrid extends React.Component {
         // CELLS // CELLS // CELLS // CELLS // CELLS
 
 
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-
- //  renderPopups() {
-
-
-
-
-
- //    const { currentMonth, selectedDate } = this.state;
- //    const monthStart = startOfMonth(currentMonth);
- //    const monthEnd = endOfMonth(monthStart);
- //    const startDate = startOfWeek(monthStart);
- //    const endDate = endOfWeek(monthEnd);
-
- //    const dateFormat = "d";
- //    const rows = [];
-
- //    let eventboxes = [];
- //    let days2 = [];
- //    let day = startDate;
- //    let formattedDate = "";
- //    let eventNames = "";
-
-
-	// this.props.calendarRecords.forEach((element) => {
-
-	//   	eventNames += element.fields.Event + " "
-
-
-	//   	 eventboxes.push(
- //          <div
- //            className={`eventbox`}
- //            id={element.id}
- //            key={day}
- //          >
- //            { element.fields.Event }
- //          </div>
- //        );
-
-
-	// });
-
- //    return <div className="eventboxes">{eventboxes}</div>;
- //  }
-
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS// POPUPS
-
 
   onDateClick = day => {
     this.setState({
